f08a05219ada3068b436079d318984e2e9281425
CyrDuck <2754899405@qq.com>
2026-04-24 23:37:54 +0800

    init: project structure and requirements

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game Collection API
+
+Personal project for XJCO3011 Web Services module.
+
+Work in progress.
---
+++ main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Game Collection API",
+    version="0.1.0"
+)
